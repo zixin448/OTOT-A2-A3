83dbed89d5d436d30b51d6b15405c5152c105f1d
Yong Zi Xin <zixinyong448@gmail.com>
2022-09-18 17:27:33 +0800

    Add demo_a3 script

---
+++ demo_a3.sh
@@ -0,0 +1,55 @@
+# ======================
+echo "demo for task A3"
+
+echo $line
+# ======================
+# ======================
+echo "Setting Up"
+
+echo $line
+# ======================
+
+echo "Installing metrics-servers"
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+
+echo $line
+
+echo "Edit the TLF Deployment manifest and add a flag `--kubelet-insecure-tls` to `deployment.spec.containers[].args[]`"
+kubectl -nkube-system edit deploy/metrics-server
+
+echo $line
+
+echo "Restarting the TLF Deloyment"
+kubectl -nkube-system rollout restart deploy/metrics-server
+
+echo $line
+
+echo "Applying Horizontal Pod Autoscaler"
+kubectl apply -f k8s/manifests/backend-horizontalpodautoscaler.yaml
+
+echo $line
+
+echo "Applying backend-zone-aware"
+kubectl apply -f k8s/manifests/backend-zone-aware.yaml
+
+echo $line
+
+# ======================
+echo "Verifying..."
+
+echo $line
+# ======================
+
+echo "Verifying HPA"
+kubectl get po
+kubectl describe HPA
+
+echo $line
+
+echo "Viewing nodes"
+kubectl get nodes -L topology.kubernetes.io/zone
+
+echo $line
+
+echo "Verifying backend-zone-aware"
+kubectl get po -lapp=backend-zone-aware -owide --sort-by='.spec.nodeName'
